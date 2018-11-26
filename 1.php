dab5ba22063d78c82449800eceb479d9430f1e39
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2018-11-26 15:27:25 +0300

    The first task has been accomplished

--- 1.php
+++ 1.php
@@ -1,4 +1,21 @@
 <?php
+
+$a = mt_rand(-10,10);
+$b = mt_rand(-10,10);
+
+echo "a = $a, b = $b <br>";
+
+if ($a >= 0 && $b >= 0) {
+    $c = $a - $b;
+    echo "Разность a и b = $c";
+} elseif ($a < 0 && $b < 0) {
+    $c = $a * $b;
+    echo "Произведение a * b = $c";
+} elseif ($a < 0 && $b >= 0 || $a >= 0 && $b < 0 ) {
+    $c = $a + $b;
+    echo "Сумма a и b = $c";
+}
+
 /**
  * Created by PhpStorm.
  * User: Alex1
