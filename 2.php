b5f118f9938e595b6e9ddbf66fd50da32ead7723
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2018-11-26 15:38:20 +0300

    Second task has been accomplished

--- 2.php
+++ 2.php
@@ -1,4 +1,60 @@
 <?php
+
+$a = mt_rand(0, 15);
+echo "$a<br>";
+
+switch ($a) {
+    case 0:
+        echo "переменная a = 0 (выведено с помощью switch)";
+        break;
+    case 1:
+        echo "переменная a = 1 (выведено с помощью switch)";
+        break;
+    case 2:
+        echo "переменная a = 2 (выведено с помощью switch)";
+        break;
+    case 3:
+        echo "переменная a = 3 (выведено с помощью switch)";
+        break;
+    case 4:
+        echo "переменная a = 4 (выведено с помощью switch)";
+        break;
+    case 5:
+        echo "переменная a = 5 (выведено с помощью switch)";
+        break;
+    case 6:
+        echo "переменная a = 6 (выведено с помощью switch)";
+        break;
+    case 7:
+        echo "переменная a = 7 (выведено с помощью switch)";
+        break;
+    case 8:
+        echo "переменная a = 8 (выведено с помощью switch)";
+        break;
+    case 9:
+        echo "переменная a = 9 (выведено с помощью switch)";
+        break;
+    case 10:
+        echo "переменная a = 10 (выведено с помощью switch)";
+        break;
+    case 11:
+        echo "переменная a = 11 (выведено с помощью switch)";
+        break;
+    case 12:
+        echo "переменная a = 12 (выведено с помощью switch)";
+        break;
+    case 13:
+        echo "переменная a = 13 (выведено с помощью switch)";
+        break;
+    case 14:
+        echo "переменная a = 14 (выведено с помощью switch)";
+        break;
+    case 15:
+        echo "переменная a = 15 (выведено с помощью switch)";
+        break;
+
+}
+
 /**
  * Created by PhpStorm.
  * User: Alex1
