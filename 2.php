60bebc5e024ba8292e3622215993461811c430fa
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2018-11-27 15:51:51 +0300

    switch has been rewritten

--- 2.php
+++ 2.php
@@ -1,57 +1,23 @@
 <?php
 
 $a = mt_rand(0, 15);
-echo "$a<br>";
+$c = 9;
+switch ($c) {
+    case  0:for ($i = 0; $i < 16; $i++) {
+        echo $i . "<br>";
+    }
+        break;
+    case  $c < 0:
+        echo "Должно_ быть указано число от 0 до 15";
+        break;
+    case $c > 15:
+        echo " Должно быть указано число от 0 до 15";
+        break;
 
-switch ($a) {
-    case 0:
-        echo "переменная a = 0 (выведено с помощью switch)";
-        break;
-    case 1:
-        echo "переменная a = 1 (выведено с помощью switch)";
-        break;
-    case 2:
-        echo "переменная a = 2 (выведено с помощью switch)";
-        break;
-    case 3:
-        echo "переменная a = 3 (выведено с помощью switch)";
-        break;
-    case 4:
-        echo "переменная a = 4 (выведено с помощью switch)";
-        break;
-    case 5:
-        echo "переменная a = 5 (выведено с помощью switch)";
-        break;
-    case 6:
-        echo "переменная a = 6 (выведено с помощью switch)";
-        break;
-    case 7:
-        echo "переменная a = 7 (выведено с помощью switch)";
-        break;
-    case 8:
-        echo "переменная a = 8 (выведено с помощью switch)";
-        break;
-    case 9:
-        echo "переменная a = 9 (выведено с помощью switch)";
-        break;
-    case 10:
-        echo "переменная a = 10 (выведено с помощью switch)";
-        break;
-    case 11:
-        echo "переменная a = 11 (выведено с помощью switch)";
-        break;
-    case 12:
-        echo "переменная a = 12 (выведено с помощью switch)";
-        break;
-    case 13:
-        echo "переменная a = 13 (выведено с помощью switch)";
-        break;
-    case 14:
-        echo "переменная a = 14 (выведено с помощью switch)";
-        break;
-    case 15:
-        echo "переменная a = 15 (выведено с помощью switch)";
-        break;
+    default:
+        for ($i = $c; $i < 16; $i++) {
+            echo $i . "<br>";
+        }
 }
 
 /**
